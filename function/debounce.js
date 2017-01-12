2226eee2eadd2cb491f082845baf1124f3dc06be
musicode <musicode@qq.com>
2017-01-12 18:42:27 +0800

    update

--- function/debounce.js
+++ function/debounce.js
@@ -9,9 +9,10 @@ import * as array from '../util/array'
  *
  * @param {Function} fn 需要节制调用的函数
  * @param {number} delay 调用的时间间隔
+ * @param {?boolean} immediate 是否立即触发
  * @return {Function}
  */
-export default function (fn, delay) {
+export default function (fn, delay, immediate) {
 
   let timer
 
@@ -19,15 +20,25 @@ export default function (fn, delay) {
 
     if (!timer) {
 
-      execute(
-        fn,
-        env.NULL,
-        array.toArray(arguments)
-      )
+      let args = array.toArray(arguments)
+      if (immediate) {
+        execute(
+          fn,
+          env.NULL,
+          args
+        )
+      }
 
       timer = setTimeout(
         function () {
           timer = env.NULL
+          if (!immediate) {
+            execute(
+              fn,
+              env.NULL,
+              args
+            )
+          }
         },
         delay
       )
