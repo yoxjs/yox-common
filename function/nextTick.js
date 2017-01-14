b48b37ef8d40452b6ed7113c77652a6bfa83e378
musicode <musicode@qq.com>
2017-01-14 22:12:53 +0800

    update

--- function/nextTick.js
+++ function/nextTick.js
@@ -6,15 +6,23 @@ let nextTick
 
 if (typeof MutationObserver === 'function') {
   nextTick = function (fn) {
-    let observer = new MutationObserver(fn)
-    let textNode = env.doc.createTextNode(char.CHAR_BLANK)
-    observer.observe(
-      textNode,
-      {
-        characterData: env.TRUE,
-      }
-    )
-    textNode.data = char.CHAR_WHITESPACE
+    // 移动端的输入法唤起时，貌似会影响 MutationObserver 的 nextTick 触发
+    // 因此当输入框是激活状态时，改用 setTimeout
+    let { activeElement } = env.doc
+    if (activeElement && 'oninput' in activeElement) {
+      setTimeout(fn)
+    }
+    else {
+      let observer = new MutationObserver(fn)
+      let textNode = env.doc.createTextNode(char.CHAR_BLANK)
+      observer.observe(
+        textNode,
+        {
+          characterData: env.TRUE,
+        }
+      )
+      textNode.data = char.CHAR_WHITESPACE
+    }
   }
 }
 else if (typeof setImmediate === 'function') {
