bd21e3dcfaff50ec0e0cd95caa66d30d142af73a
musicode <musicode@qq.com>
2017-04-17 19:41:08 +0800

    update

--- function/matchFirst.js
+++ function/matchFirst.js
@@ -1,18 +1,19 @@
 
+import * as env from '../util/env'
 import * as array from '../util/array'
 import * as string from '../util/string'
 
 /**
  * 用前缀匹配数组中的第一个字符串
  *
- * @param {Array.<string>} array
+ * @param {Array.<string>} list
  * @param {string} value
  * @return {Array}
  */
-export default function (array, value) {
+export default function (list, value) {
   let result = [ ]
   array.each(
-    array,
+    list,
     function (prefix) {
       if (string.startsWith(value, prefix)) {
         array.push(
