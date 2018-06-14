0e73fa3dc9d35b5343113be7a72dd6e3a25261ae
musicode <musicode@qq.com>
2018-06-14 11:33:08 +0800

    update

--- util/keypath.js
+++ util/keypath.js
@@ -5,25 +5,12 @@ import * as char from './char'
 import * as array from './array'
 import * as string from './string'
 
-const normalizeCache = { }
-
 export function normalize(str) {
   if (!string.falsy(str)) {
     let start = string.indexOf(str, char.CHAR_OBRACK)
     if (start > 0 && string.indexOf(str, char.CHAR_CBRACK) > start) {
-      if (!normalizeCache[ str ]) {
-        normalizeCache[ str ] = str.replace(
-          /\[\s*?([^\]]+)\s*?\]/g,
-          function ($0, $1) {
-            let code = char.codeAt($1)
-            if (code === char.CODE_SQUOTE || code === char.CODE_DQUOTE) {
-              $1 = string.slice($1, 1, -1)
-            }
-            return env.KEYPATH_SEPARATOR + $1
-          }
-        )
-      }
-      return normalizeCache[ str ]
+      // 临时代码，提醒一段时间就删掉
+      console.error('不支持 "${str}" 写法')
     }
   }
   return str
