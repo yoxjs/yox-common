aae15f929d42ecf6aea28ca15718a5f9439779ce
musicode <musicode@qq.com>
2019-04-28 14:08:11 +0800

    update

--- src/util/Event.ts
+++ src/util/Event.ts
@@ -16,7 +16,7 @@ export default class CustomEvent implements CustomEventInterface {
   // 是否已阻止事件的默认行为
   isPrevented?: boolean
 
-  // 事件是否已停止冒泡
+  // 是否已停止事件冒泡
   isStoped?: boolean
 
   // 处理当前事件的监听器，方便外部获取 listener 进行解绑
