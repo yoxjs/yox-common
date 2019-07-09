0f32dd4fcbb50dc3d1dac8c947ea0a9348dde163
musicode <musicode@qq.com>
2019-07-09 15:01:27 +0800

    调用 off 如果 listener 传入空，打印警告

--- src/util/Emitter.ts
+++ src/util/Emitter.ts
@@ -219,6 +219,14 @@ export default class Emitter implements EmitterInterface {
         object.each(listeners, each)
       }
 
+      // 在开发阶段进行警告，比如传了 listener 进来，listener 是个空值
+      // 但你不知道它是空值
+      if (process.env.NODE_ENV === 'development') {
+        if (arguments.length > 1) {
+          logger.warn(`emitter.off(type, listener) is invoked, but "listener" is ${listener}.`)
+        }
+      }
+
     }
     else {
       // 清空
