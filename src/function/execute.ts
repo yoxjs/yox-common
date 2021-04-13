508c5578825deb93c7f4bec10bd88d305a39a22c
musicode <musicode@qq.com>
2021-04-13 19:35:27 +0800

    Update execute.ts

--- src/function/execute.ts
+++ src/function/execute.ts
@@ -9,14 +9,12 @@ import * as constant from '../util/constant'
  * @param args 调用函数的参数，多参数时传入数组
  * @return 调用函数的返回值
  */
-export default function (fn: any, context?: any, args?: any): any {
-  if (is.func(fn)) {
-    return is.array(args)
-      ? fn.apply(context, args)
-      : context !== constant.UNDEFINED
-        ? fn.call(context, args)
-        : args !== constant.UNDEFINED
-          ? fn(args)
-          : fn()
-  }
+export default function (fn: Function, context?: any, args?: any): any {
+  return is.array(args)
+    ? fn.apply(context, args)
+    : context !== constant.UNDEFINED
+      ? fn.call(context, args)
+      : args !== constant.UNDEFINED
+        ? fn(args)
+        : fn()
 }
